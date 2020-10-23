4f57d17a50c61bb6ed4ae097eaa848414a4e3a47
RedWizard22 <pviegasuk@gmail.com>
2020-10-23 10:38:11 +0100

    made hardlinks

--- makehardlink.sh
+++ makehardlink.sh
@@ -3,5 +3,5 @@
 rm ./repo/x86_64/rpac.db
 rm ./repo/x86_64/rpac.files
 
-ln rpac.db.tar.gz ./repo/x86_64/rpac.db
-ln rpac.files.tar.gz ./repo/x86_64/rpac.files
+ln ./repo/x86_64/rpac.db.tar.gz ./repo/x86_64/rpac.db
+ln ./repo/x86_64/rpac.files.tar.gz ./repo/x86_64/rpac.files
